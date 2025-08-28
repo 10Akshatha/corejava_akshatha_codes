abf17aaed9a3d29abf62f637a697e6e49a6b356d
akshatha <akshathads.22ds@saividya.ac.in>
2025-08-28 20:04:56 +0530

    Sixth Commit

---
+++ corejava_tnsif/src/Static/StaticMethod.java
@@ -0,0 +1,18 @@
+package Static;
+class PaymentGateway
+{
+	static void showSupportedBanks()
+	{
+		System.out.println("Supported Banks : SBI, HDFC,ICICI,AXis");
+	}
+}
+
+
+public class StaticMethod {
+
+	public static void main(String[] args) {
+		PaymentGateway.showSupportedBanks();
+
+	}
+
+}
